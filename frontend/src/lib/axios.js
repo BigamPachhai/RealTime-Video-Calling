2e19d39971c7c96c24c015ba14efcb073095ec74
Bigam Pachhai <bigampachhai2@gmail.com>
2025-10-06 07:43:05 +0545

    fix

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 
-const BASE_URL =
-  import.meta.env.MODE === "development"
-    ? "http://localhost:5001/api"
-    : "https://videocallbackned.vercel.app/api";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : import.meta.env.BACKEND_URL + "/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
